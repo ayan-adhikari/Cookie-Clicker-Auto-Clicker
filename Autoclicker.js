cd3a261bc72bf3b5f1c3eae71642b6db4c45ef0b
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-02-13 14:27:51 +0800

    Feat: add click function and assign cookie id

---
+++ Autoclicker.js
@@ -0,0 +1,10 @@
+//Assigning elements to variables
+const cookie = document.getElementById("bigCookie")
+function click(button,x)
+{
+    for (let i=0; i<x; i++)
+    {
+        button.click()
+        setTimeout(10, a)
+    }
+}
