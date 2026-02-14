3d0253aaf562b3ce921c5fea1546209a5e4abe65
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-02-15 07:56:51 +0800

    Feat: add ability to click cookie fast

--- Autoclicker.js
+++ Autoclicker.js
@@ -1,10 +1,55 @@
 //Assigning elements to variables
-const cookie = document.getElementById("bigCookie")
-function click(button,x)
+const cookie = document.getElementById(`bigCookie`)
+//Other variables
+let randInt
+let clicks = 500
+let cookieInterval
+//Random number function
+function generateRandomNumber(min, max) 
 {
-    for (let i=0; i<x; i++)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//Filler function
+function a()
+{
+    console.log()
+}
+//Function to click cookie/building/upgrades
+function click()
+{
+    Game.ClickCookie()
+    for (let i = 0; i++; i>=20)
     {
-        button.click()
-        setTimeout(10, a)
+        document.getElementById(`product${i}`).click()
+    }
+    randInt = generateRandomNumber()
+    setTimeout(click, randInt)
+}
+
+//Function to buy buildings
+/*
+function buyBuilding(x)
+{
+    let i = 0
+    while (i <= x)
+    {
+        const building = document.getElementById(`product${}`)
+        setTimeout(a, 10)
+        console.log(`${button} clicked: ${i} times.`)
+        i += 1
     }
 }
+//Function to buy upgrades
+
+function buyUpgrade(x)
+{
+    let i = 0
+    while (i <= x)
+    {
+
+        setTimeout(a, 10)
+        console.log(`${building} clicked: ${i} times.`)
+        i += 1
+    }
+}
+    */
